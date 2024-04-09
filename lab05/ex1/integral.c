1646641493406fe46782fa9fef06eb83a60f851f
krudny <k.rudny1@gmail.com>
2024-04-09 21:44:41 +0200

    lab5 finished

--- lab05/ex1/integral.c
+++ lab05/ex1/integral.c
@@ -2,9 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <math.h>
 
--- lab05/ex2/integral_pipe_specs.h
+++ lab05/ex2/integral_pipe_specs.h
@@ -1,14 +1,18 @@
 #ifndef INTEGRAL_PIPE_SPECS_H
 #define INTEGRAL_PIPE_SPECS_H
 
-const char* in_pipe_name = "int_calc_in.fifo";
-const char* out_pipe_name = "int_calc_out.fifo";
+typedef unsigned long long ull;
 
+/** name of the pipes */
+#define IN_PIPE_NAME "int_calc_in.fifo"
+#define OUT_PIPE_NAME "int_calc_out.fifo"
+
+/** struct for the data to be pushed */
 typedef struct {
     double range_start;
     double range_stop;
 
-    long number_of_intervals;
+    ull number_of_intervals;
 } integral_range_t;
 
 #endif //INTEGRAL_PIPE_SPECS_H
